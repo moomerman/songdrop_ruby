51e779ab0c509f7d2ee191fee54a5cea1bca36bf
Richard Taylor <moomerman@gmail.com>
2013-07-09 18:17:23 +0100

    fleshed out the bubblewrap http and json handler

--- lib/songdrop/http/bubblewrap.rb
+++ lib/songdrop/http/bubblewrap.rb
@@ -1,4 +1,43 @@
 module Songdrop
   class HTTP
+
+    def self.get(url, params={}, &block)
+      http_request(:get, url, params, &block)
+    end
+
+    def self.post(url, params={}, &block)
+      http_request(:post, url, params, &block)
+    end
+
+    def self.put(url, params={}, &block)
+      http_request(:put, url, params, &block)
+    end
+
+    def self.delete(url, params={}, &block)
+      http_request(:delete, url, params, &block)
+    end
+
+    def http_request(method, url, options={}, &block)
+      options[:payload] ||= {}
+      options.merge!({:headers => {:Accept => "application/json"}})
+      options[:payload].merge!(:client => client_token)
+      options[:payload].merge!(:token => User.current_user.combined_api_token) if User.current_user
+
+      puts "[SongdropAPI] #{method} #{url} with: #{options.inspect}"
+
+      BubbleWrap::HTTP.send(method, url, options) do |response|
+        if response.ok?
+          begin #in case we don't receive JSON back, we don't want the app to crash:
+            json = BubbleWrap::JSON.parse(response.body.to_str)
+            block.call response, json
+          rescue
+            block.call response, nil
+          end
+        else
+          block.call response, nil
+        end
+      end
+    end
+
   end
 end
--- lib/songdrop/json/bubblewrap.rb
+++ lib/songdrop/json/bubblewrap.rb
@@ -1,1 +1,9 @@
-bubblewrap.rb
+module Songdrop
+  class JSON
+
+    def self.parse(json, &block)
+      block.call ::BubbleWrap::JSON.parse(json)
+    end
+
+  end
+end

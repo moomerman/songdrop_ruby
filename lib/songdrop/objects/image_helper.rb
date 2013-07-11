59482716b720f925d7e16598226563f2fc1da71a
Richard Taylor <moomerman@gmail.com>
2013-07-11 13:47:58 +0100

    changed image_url <-> image to be consistent with the website

--- lib/songdrop/objects/image_helper.rb
+++ lib/songdrop/objects/image_helper.rb
@@ -1,10 +1,10 @@
 module Songdrop
   module ImageHelper
 
-    def image_url(options={})
+    def image(options={})
       options[:action] ||= 'crop'
       options[:size] ||= '50x50'
-      self.image.gsub('ACTION', options[:action]).gsub('WIDTHxHEIGHT', options[:size])
+      self.image_url.gsub('ACTION', options[:action]).gsub('WIDTHxHEIGHT', options[:size])
     end
 
   end
